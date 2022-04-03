777bd93b7c44dcc42ea02ab0a733c658eb5e113b
Colin Dean <colin.dean@target.com>
2022-04-02 20:10:02 -0400

    pdfjam: address brew audit recommendations

--- pdfjam.rb
+++ pdfjam.rb
@@ -2,17 +2,17 @@ class Pdfjam < Formula
   desc "Simple shell interface to the pdfpages package for pdfLaTeX"
   homepage "https://github.com/rrthomas/pdfjam"
   url "https://github.com/rrthomas/pdfjam/archive/refs/tags/v3.03.tar.gz"
-  head "https://github.com/rrthomas/pdfjam.git"
   sha256 "bd27e44e75909cac2a53f0c8d0b253d9c95e496a181b7837f7919724dff78b69"
+  head "https://github.com/rrthomas/pdfjam.git"
 
   depends_on "gnu-sed" => :build
 
   def install
     if build.head?
       opoo "Building from HEAD may not work because of GNU/BSD sed differences"
-      ENV["PATH"] = Formula["gnu-sed"].libexec/"gnubin" + ":" + ENV["PATH"]
+      ENV["PATH"] = "#{Formula["gnu-sed"].libexec/"gnubin"}:#{ENV["PATH"]}"
 
-      open(buildpath/"VERSION", 'a') do |f|
+      File.open(buildpath/"VERSION", "a") do |f|
         f.write("-#{version}")
       end
 
@@ -29,12 +29,13 @@ class Pdfjam < Formula
     etc.install(build_dir/"pdfjam.conf")
   end
 
-  def caveats; <<~EOS
-    pdfjam requires the installation of pdfLaTeX. You may need to install
-    TeX Live. Both MacTeX and BasicTeX are acceptable.
+  def caveats
+    <<~EOS
+      pdfjam requires the installation of pdfLaTeX. You may need to install
+      TeX Live. Both MacTeX and BasicTeX are acceptable.
 
-    brew install mactex
-    brew install basictex
+      brew install mactex
+      brew install basictex
     EOS
   end
 
